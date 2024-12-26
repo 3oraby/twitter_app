d39f4e5eaff2b57555229553299110dd49a9ab5c
3oraby <mohaahmed21102003@gmail.com>
2024-12-26 04:48:08 +0200

    make the drawer

--- lib/core/widgets/custom_app_drawer.dart
+++ lib/core/widgets/custom_app_drawer.dart
@@ -1,5 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:twitter_app/core/constants/app_constants.dart';
+import 'package:twitter_app/core/widgets/custom_drawer_list_tile_option.dart';
+import 'package:twitter_app/core/widgets/vertical_gap.dart';
 
 class CustomAppDrawer extends StatefulWidget {
   const CustomAppDrawer({
@@ -9,7 +12,6 @@ class CustomAppDrawer extends StatefulWidget {
   @override
   State<CustomAppDrawer> createState() => _CustomAppDrawerState();
 }
-
 
 class _CustomAppDrawerState extends State<CustomAppDrawer> {
   late bool switchValue;
@@ -28,22 +30,59 @@ class _CustomAppDrawerState extends State<CustomAppDrawer> {
   Widget build(BuildContext context) {
     return Drawer(
       child: SafeArea(
-        child: Column(
-          children: [
-            Switch(
-              value: switchValue,
-              onChanged: (value) async {
-                if (value) {
-                  await context.setLocale(const Locale('ar'));
-                } else {
-                  await context.setLocale(const Locale('en'));
-                }
-                setState(() {
-                  switchValue = value;
-                });
-              },
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: AppConstants.horizontalPadding,
+          ),
+          child: Column(
+            children: [
+              CustomDrawerListTileOption(
+                title: context.tr("Profile"),
+                iconData: Icons.person_2_outlined,
+                onTap: () {},
+              ),
+              const VerticalGap(8),
+              CustomDrawerListTileOption(
+                title: context.tr("Bookmarks"),
+                iconData: Icons.bookmark_border,
+                onTap: () {},
+              ),
+              const VerticalGap(8),
+              CustomDrawerListTileOption(
+                title: context.tr("Settings"),
+                iconData: Icons.settings,
+                onTap: () {},
+              ),
+              const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.wb_sunny,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+              const VerticalGap(24),
+              Switch(
+                value: switchValue,
+                onChanged: (value) async {
+                  if (value) {
+                    await context.setLocale(const Locale('ar'));
+                  } else {
+                    await context.setLocale(const Locale('en'));
+                  }
+                  setState(() {
+                    switchValue = value;
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/core/widgets/custom_drawer_list_tile_option.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:twitter_app/core/utils/app_colors.dart';
+import 'package:twitter_app/core/utils/app_text_styles.dart';
+
+class CustomDrawerListTileOption extends StatelessWidget {
+  const CustomDrawerListTileOption({
+    super.key,
+    required this.title,
+    required this.iconData,
+    required this.onTap,
+  });
+
+  final String title;
+  final IconData iconData;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+
+      onTap: onTap,
+      contentPadding: EdgeInsets.zero,
+      leading: Icon(
+        iconData,
+        color: AppColors.primaryColor,
+        size: 30,
+      ),
+      title: Text(
+        title,
+        style: AppTextStyles.uberMoveBlack24,
+      ),
+    );
+  }
+}
