32456463a3370ae6e6fc33b457cd7835f62c9e15
3oraby <mohaahmed21102003@gmail.com>
2025-01-30 06:30:34 +0200

    loading the page again while pop from

--- lib/features/home/presentation/widgets/for_you_tab_bar_body.dart
+++ lib/features/home/presentation/widgets/for_you_tab_bar_body.dart
@@ -1,34 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:twitter_app/core/utils/app_colors.dart';
+import 'package:twitter_app/features/tweet/presentation/cubits/get_tweets_cubit/get_tweets_cubit.dart';
 import 'package:twitter_app/features/tweet/presentation/widgets/custom_tweet_info_card.dart';
 import 'package:twitter_app/core/widgets/vertical_gap.dart';
 import 'package:twitter_app/features/comments/presentation/screens/show_tweet_comments_screen.dart';
 import 'package:twitter_app/features/tweet/domain/entities/tweet_details_entity.dart';
 
-class ForYouTabBarBody extends StatelessWidget {
+class ForYouTabBarBody extends StatefulWidget {
   const ForYouTabBarBody({
     super.key,
     required this.tweets,
   });
   final List<TweetDetailsEntity> tweets;
+
+  @override
+  State<ForYouTabBarBody> createState() => _ForYouTabBarBodyState();
+}
+
+class _ForYouTabBarBodyState extends State<ForYouTabBarBody> {
+  _refreshPage() {
+    BlocProvider.of<GetTweetsCubit>(context).getTweets();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
         children: [
           const VerticalGap(24),
-          for (int index = 0; index < tweets.length; index++) ...[
+          for (int index = 0; index < widget.tweets.length; index++) ...[
             CustomTweetInfoCard(
-              tweetDetailsEntity: tweets[index],
+              tweetDetailsEntity: widget.tweets[index],
               onTweetTap: () {
                 Navigator.pushNamed(
                   context,
                   ShowTweetCommentsScreen.routeId,
-                  arguments: tweets[index],
+                  arguments: widget.tweets[index],
+                ).then(
+                  (value) {
+                    _refreshPage();
+                  },
                 );
               },
             ),
-            if (index != tweets.length - 1)
+            if (index != widget.tweets.length - 1)
               const Divider(
                 color: AppColors.dividerColor,
                 height: 24,
