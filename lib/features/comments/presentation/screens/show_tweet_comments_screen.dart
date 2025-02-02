ffaa4fde28ef9b65eb2c1a33140ef048e1c85447
3oraby <mohaahmed21102003@gmail.com>
2025-02-02 20:50:05 +0200

    make reply ui

--- lib/features/comments/presentation/screens/show_tweet_comments_screen.dart
+++ lib/features/comments/presentation/screens/show_tweet_comments_screen.dart
@@ -1,16 +1,17 @@
-
 import 'package:flutter/material.dart';
 import 'package:twitter_app/core/constants/app_constants.dart';
 import 'package:twitter_app/core/helpers/functions/build_custom_app_bar.dart';
+import 'package:twitter_app/core/helpers/functions/get_current_user_entity.dart';
 import 'package:twitter_app/core/utils/app_colors.dart';
 import 'package:twitter_app/core/utils/app_text_styles.dart';
 import 'package:twitter_app/core/widgets/vertical_gap.dart';
+import 'package:twitter_app/features/auth/domain/entities/user_entity.dart';
 import 'package:twitter_app/features/comments/presentation/widgets/custom_make_reply_section.dart';
 import 'package:twitter_app/features/comments/presentation/widgets/show_tweet_comments_part.dart';
 import 'package:twitter_app/features/tweet/presentation/widgets/custom_main_details_tweet_card.dart';
 import 'package:twitter_app/features/tweet/domain/entities/tweet_details_entity.dart';
 
-class ShowTweetCommentsScreen extends StatelessWidget {
+class ShowTweetCommentsScreen extends StatefulWidget {
   const ShowTweetCommentsScreen({
     super.key,
     required this.tweetDetailsEntity,
@@ -18,6 +19,28 @@ class ShowTweetCommentsScreen extends StatelessWidget {
 
   static const String routeId = 'kShowTweetCommentsScreen';
   final TweetDetailsEntity tweetDetailsEntity;
+
+  @override
+  _ShowTweetCommentsScreenState createState() =>
+      _ShowTweetCommentsScreenState();
+}
+
+class _ShowTweetCommentsScreenState extends State<ShowTweetCommentsScreen> {
+  bool isSectionExpanded = false;
+  late UserEntity currentUser;
+  final ScrollController _scrollController = ScrollController();
+
+  void toggleSection() {
+    setState(() {
+      isSectionExpanded = !isSectionExpanded;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    currentUser = getCurrentUserEntity();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,111 +52,48 @@ class ShowTweetCommentsScreen extends StatelessWidget {
           style: AppTextStyles.uberMoveBlack20,
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: AppConstants.horizontalPadding,
-        ),
-        child: Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    CustomMainDetailsTweetCard(
-                      tweetDetailsEntity: tweetDetailsEntity,
-                    ),
-                    const Divider(
-                      color: AppColors.dividerColor,
-                      height: 32,
-                    ),
-                    ShowTweetCommentsPart(
-                        tweetDetailsEntity: tweetDetailsEntity),
-                  ],
+      body: GestureDetector(
+        onTap: () {
+          if (isSectionExpanded) {
+            toggleSection();
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: AppConstants.horizontalPadding,
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  controller: _scrollController,
+                  child: Column(
+                    children: [
+                      CustomMainDetailsTweetCard(
+                        tweetDetailsEntity: widget.tweetDetailsEntity,
+                      ),
+                      const Divider(
+                        color: AppColors.dividerColor,
+                        height: 32,
+                      ),
+                      ShowTweetCommentsPart(
+                        tweetDetailsEntity: widget.tweetDetailsEntity,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            CustomMakeReplySection(
-              tweetDetailsEntity: tweetDetailsEntity,
-            ),
-            const VerticalGap(16),
-          ],
+              CustomMakeReplySection(
+                tweetDetailsEntity: widget.tweetDetailsEntity,
+                isSectionExpanded: isSectionExpanded,
+                onTextFieldTap: toggleSection,
+                currentUser: currentUser,
+              ),
+              const VerticalGap(16),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
-
-// class ExpandedMakeReplySection extends StatefulWidget {
-//   const ExpandedMakeReplySection({
-//     super.key,
-//     required this.currentUser,
-//     required this.tweetDetailsEntity,
-//   });
-//   final UserEntity currentUser;
-//   final TweetDetailsEntity tweetDetailsEntity;
-
-//   @override
-//   State<ExpandedMakeReplySection> createState() =>
-//       _ExpandedMakeReplySectionState();
-// }
-
-// class _ExpandedMakeReplySectionState extends State<ExpandedMakeReplySection> {
-//   late FocusNode _focusNode;
-//   @override
-//   void initState() {
-//     super.initState();
-//     _focusNode = FocusNode();
-//     // Delay focus request to ensure the widget is fully built
-//     WidgetsBinding.instance.addPostFrameCallback((_) {
-//       _focusNode.requestFocus();
-//     });
-//   }
-
-//   @override
-//   void dispose() {
-//     _focusNode.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SlidingUpPanel(
-//       body: SingleChildScrollView(
-//         child: Column(
-//           children: [
-//             ListTile(
-//               contentPadding: EdgeInsets.all(0),
-//               leading: BuildUserCircleAvatarImage(
-//                 profilePicUrl: widget.currentUser.profilePicUrl,
-//                 circleAvatarRadius: 20,
-//               ),
-//               title: Text(
-//                 "${widget.currentUser.firstName} ${widget.currentUser.lastName}",
-//               ),
-//               subtitle: Text(widget.currentUser.email),
-//             ),
-//             VerticalGap(4),
-//             Row(
-//               children: [
-//                 Text("Replying to"),
-//                 Text("@${widget.currentUser.email}"),
-//               ],
-//             ),
-//             VerticalGap(12),
-//             CustomTextFormFieldWidget(
-//               hintText: "Post your reply",
-//               contentPadding: 16,
-//               focusNode: _focusNode,
-//             ),
-//             VerticalGap(8),
-//             Row(
-//               children: [],
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
--- lib/features/comments/presentation/widgets/custom_make_reply_section.dart
+++ lib/features/comments/presentation/widgets/custom_make_reply_section.dart
@@ -1,44 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:twitter_app/core/helpers/functions/get_current_user_entity.dart';
 import 'package:twitter_app/features/auth/domain/entities/user_entity.dart';
 import 'package:twitter_app/features/comments/presentation/widgets/collapsed_make_reply_section.dart';
 import 'package:twitter_app/features/comments/presentation/widgets/expanded_make_reply_section.dart';
 import 'package:twitter_app/features/tweet/domain/entities/tweet_details_entity.dart';
 
-class CustomMakeReplySection extends StatefulWidget {
+class CustomMakeReplySection extends StatelessWidget {
   const CustomMakeReplySection({
     super.key,
     required this.tweetDetailsEntity,
+    required this.isSectionExpanded,
+    required this.onTextFieldTap,
+    required this.currentUser,
   });
-  final TweetDetailsEntity tweetDetailsEntity;
-  @override
-  State<CustomMakeReplySection> createState() => _CustomMakeReplySectionState();
-}
 
-class _CustomMakeReplySectionState extends State<CustomMakeReplySection> {
-  late UserEntity currentUser;
-  bool isSectionExpanded = false;
-  @override
-  void initState() {
-    super.initState();
-    currentUser = getCurrentUserEntity();
-  }
+  final TweetDetailsEntity tweetDetailsEntity;
+  final bool isSectionExpanded;
+  final VoidCallback onTextFieldTap;
+  final UserEntity currentUser;
 
   @override
   Widget build(BuildContext context) {
     if (isSectionExpanded) {
-      return ExpandedMakeReplySection(
-        currentUser: currentUser,
-        tweetDetailsEntity: widget.tweetDetailsEntity,
+      return GestureDetector(
+        onTap: () {},
+        child: ExpandedMakeReplySection(
+          currentUser: currentUser,
+          tweetDetailsEntity: tweetDetailsEntity,
+        ),
       );
     } else {
       return CollapsedMakeReplySection(
         currentUser: currentUser,
-        onTextFieldTap: () {
-          setState(() {
-            isSectionExpanded = true;
-          });
-        },
+        onTextFieldTap: onTextFieldTap,
       );
     }
   }
